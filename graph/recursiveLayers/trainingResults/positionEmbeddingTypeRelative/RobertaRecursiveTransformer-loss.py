86647e6d64957806bfe1be1a0a7cab875b1c5ee3
Richard Baxter <richard.baxter@baxterai.com>
2023-09-27 01:13:05 +1000

    update graph positionEmbeddingTypeRelative/RobertaRecursiveTransformer - start recursiveLayersNormaliseNumParameters ("norm")

--- graph/recursiveLayers/trainingResults/positionEmbeddingTypeRelative/RobertaRecursiveTransformer-loss.py
+++ graph/recursiveLayers/trainingResults/positionEmbeddingTypeRelative/RobertaRecursiveTransformer-loss.py
@@ -7,12 +7,16 @@ y2 = np.array([0.5, 0.30940675837397574, 0.28117460092902186, 0.2687879767358303
 y3 = np.array([0.5, 0.30296802766025066, 0.28217491447329524, 0.27392400919795035, 0.2656524788528681])
 y4 = np.array([0.5, 0.2995814057946205, 0.27438519304990766, 0.2595572123140097, 0.2537853354871273])
 y5 = np.array([0.5, 0.36026279502511027, 0.34200011098980904, 0.3328074170410633, 0.32603148027658463])
+y6 = np.array([0.5, 0.2982051469832659, np.nan, np.nan, np.nan])
+y7 = np.array([0.5, 0.3598037402355671, np.nan, np.nan, np.nan])
 
 l1, = plt.plot(x, y1, color='green', label='lay=1, hid=768, head=12 (120MB)')
 l2, = plt.plot(x, y2, color='blue', label='lay=6, hid=768, head=12 (256MB)')
 l3, = plt.plot(x, y3, color='red', label='lay=1r6, hid=768, head=12 (120MB)')
 l4, = plt.plot(x, y4, color='pink', label='lay=1r6, hid=1536, head=24 (non-intermediate norm:263MB)', linestyle='dashed')
 l5, = plt.plot(x, y5, color='lightgreen', label='lay=1, hid=1536, head=24 (non-intermediate norm:263MB)', linestyle='dashed')
+l6, = plt.plot(x, y6, color='pink', label='lay=1r6, hid=1344, head=21 (norm:249MB)')
+l7, = plt.plot(x, y7, color='lightgreen', label='lay=1, hid=1344, head=21 (norm:249MB)')
 
 plt.xticks(np.arange(min(x), max(x)+1, 1.0))
 plt.yticks(np.arange(0, 0.5+0.1, 0.1))
@@ -21,6 +25,6 @@ plt.xlabel("number of oscar en train samples (x1000000)")
 plt.ylabel("Masked LM test loss")
 plt.title("RoBERTa Recursive Transformer")
 
-plt.legend(handles=[l2, l3, l4, l1, l5])
+plt.legend(handles=[l2, l3, l4, l6, l1, l5, l7])
 
 plt.show()
--- graph/recursiveLayers/trainingResults/positionEmbeddingTypeRelative/RobertaRecursiveTransformer.py
+++ graph/recursiveLayers/trainingResults/positionEmbeddingTypeRelative/RobertaRecursiveTransformer.py
@@ -7,12 +7,16 @@ y2 = np.array([0, 0.42890446846485136, 0.4656440287351608, 0.48165937175750734, 
 y3 = np.array([0, 0.44220867459774016, 0.4677618439912796, 0.4787390529155731, 0.49010481104850767])
 y4 = np.array([0, 0.4473719531536102, 0.47961312062740324, 0.49951700699329377, 0.5067597522258759])
 y5 = np.array([0, 0.3397151798963547, 0.3601354651093483, 0.3720378009200096, 0.3802973414897919])
+y6 = np.array([0, 0.44768228613615035, np.nan, np.nan, np.nan])
+y7 = np.array([0, 0.3375083382666111, np.nan, np.nan, np.nan])
 
 l1, = plt.plot(x, y1, color='green', label='lay=1, hid=768, head=12 (120MB)')
 l2, = plt.plot(x, y2, color='blue', label='lay=6, hid=768, head=12 (256MB)')
 l3, = plt.plot(x, y3, color='red', label='lay=1r6, hid=768, head=12 (120MB)')
 l4, = plt.plot(x, y4, color='pink', label='lay=1r6, hid=1536, head=24 (non-intermediate norm:263MB)', linestyle='dashed')
 l5, = plt.plot(x, y5, color='lightgreen', label='lay=1, hid=1536, head=24 (non-intermediate norm:263MB)', linestyle='dashed')
+l6, = plt.plot(x, y6, color='pink', label='lay=1r6, hid=1344, head=21 (norm:249MB)')
+l7, = plt.plot(x, y7, color='lightgreen', label='lay=1, hid=1344, head=21 (norm:249MB)')
 
 plt.xticks(np.arange(min(x), max(x)+1, 1.0))
 plt.yticks(np.arange(0, 0.5+0.1, 0.1))
@@ -21,6 +25,6 @@ plt.xlabel("number of oscar en train samples (x1000000)")
 plt.ylabel("Masked LM test accuracy (Top-1)")
 plt.title("RoBERTa Recursive Transformer")
 
-plt.legend(handles=[l2, l3, l4, l1, l5], loc='lower left')
+plt.legend(handles=[l2, l3, l4, l6, l1, l5, l7], loc='lower left')
 
 plt.show()
